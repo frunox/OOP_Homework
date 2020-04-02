b8594bf7836edf64036fdcf07f176ff4e311c054
frunox <60527588+frunox@users.noreply.github.com>
2020-04-01 20:15:56 -0500

    add employee name and id validations

--- Develop/app.js
+++ Develop/app.js
@@ -1,172 +1,167 @@
+// pull in needed reference files using require
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+// require node packages 'path' and 'fs', and npm package 'inquirer'
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+//  constant creating a folder /develop/output as the location for team.html
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+//  constant defining the path as /develop/output, and to define the name of
+//  of the file to be created at team.html
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
+// define an array to receive the information for each employee entered below
+let html = [];
+
+//  set render to use the code in htmlRenderer.js, which creates the html elements to be 
+//  included in team.html
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-class Team {
-  constructor(){
-    
 
-  }
-
-
-  start(){
-    inquirer
-    .prompt([
-      {
-        type: 'rawlist',
-        message: 'Select an action:',
-        name: 'action',
-        choices: [
-          'Add a Manager',
-          'Add an Engineer',
-          'Add an Intern',
-          'Exit Application'
-        ]
-      },
-    ]).then(({ action }) => {
-      switch (action) {
-        case 'Add a Manager':
-          this.addManager();
-          break;
-        case 'Add an Engineer':
-          this.addEngineer();
-          break;
-        case 'Add an Intern':
-          this.addIntern();
-          break;
-        default:
-          process.exit();
+// function to prompt the user to enter employee information and push it to array html[]
+function start() {
+  // define the questions to be asked of the user to enter employee data
+  const questions = [
+    // provide validation for the employee name, id and email responses
+    {
+      type: "input",
+      message: "Please enter the employee name",
+      name: "name",
+      validate: answer => {
+        if (answer === "") {
+          return "Please enter an employee name";
+        }
+        return true;
       }
-    });
-  }
-
-  addManager(){
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: 'Enter Name:',
-        name: 'name'      
-      },
-      {
-        type: 'input',
-        message: 'Enter ID #:',
-        name: 'id'
-      },
-      {
-        type: 'input',
-        message: 'Enter e-mail address:',
-        name: 'email'
-      },
-      {
-        type: 'input',
-        message: 'Enter office phone number:',
-        name: 'officeNumber'
+    },
+    // require the ID number to have 4 digits
+    {
+      type: "input",
+      message: "Please enter the 4-digit employee ID number",
+      name: "id",
+      validate: answer => {
+        if (answer.length !== 4) {
+          return "Please enter a valid employee id number";
+        }
+        return true;
       }
-    ]).then(async ({ name, id, email, officeNumber }) => {
-      
-      
-      
-      this.start()
-    })
-  };
+    },
+    {
+      type: "input",
+      message: "Please enter the email",
+      name: "email",
+      validate: answer => {
+        const pass = answer.match(
+          /\S+@\S+\.\S+/
+        );
+        if (pass) {
+          return true;
+        }
+        return "Please enter a valid email address";
+      }
+    },
 
-  addEngineer(){
-    inquirer
+    {
+      type: "list",
+      message: "Please select the role",
+      choices: ["Manager", "Engineer", "Intern"],
+      name: "role"
+    },
+
+    {
+      type: "input",
+      message: "Please enter the office number",
+      name: "officeNumber",
+      when: function (answers) {
+        return answers.role === "Manager";
+      }
+    },
+
+    {
+      type: "input",
+      message: "Please enter the github username",
+      name: "github",
+      when: function (answers) {
+        return answers.role === "Engineer";
+      }
+    },
+
+    {
+      type: "input",
+      message: "Please enter the school name",
+      name: "school",
+      when: function (answers) {
+        return answers.role === "Intern";
+      }
+    }
+  ]
+
+
+  // use inquirer to gather information about the development team members by prompting the questions variable
+  inquirer
+    .prompt(questions)
+    // use .then promise and feed in the parameters of name, id, email, role, officeNumber, github, and school
+    // as appropriate for each employee and their role
+    .then(function (answers) {
+      // the switch statement collects the appropriate information for each type of employee and
+      // pushes that to the array html[]
+      switch (answers.role) {
+        case "Manager":
+          const manager = new Manager(answers.name, parseInt(answers.id), answers.email, answers.officeNumber);
+          html.push(manager);
+          //
+          break
+        case "Engineer":
+          const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+          html.push(engineer);
+          break
+        case "Intern":
+          const intern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
+          html.push(intern);
+          break
+      }
+
+      // call a function to prompt if the user wants to add another employee
+      addAnother();
+    })
+}
+
+
+// define a function addEmployee() to ask to add another
+function addAnother() {
+  inquirer
     .prompt([
       {
-        type: 'input',
-        message: 'Enter Name:',
-        name: 'name'      
-      },
-      {
-        type: 'input',
-        message: 'Enter ID #:',
-        name: 'id'
-      },
-      {
-        type: 'input',
-        message: 'Enter e-mail address:',
-        name: 'email'
-      },
-      {
-        type: 'input',
-        message: 'Enter GitHub username',
-        name: 'gitHub'
-      }    
-    ]).then(async ({ name, id, email, gitHub }) => {
-
-
-      this.start()
+        type: 'confirm',
+        message: 'Do you want to add another Employee?',
+        name: 'another',
+      }
+    ])
+    .then(function ({ another }) {
+      // if the user wants to add another employee, run start()
+      if (another) {
+        start();
+      } else {
+        // console.log(html)
+        // if the user is done adding employees, call createHTML(), passing in array html[]
+        // with all the employee information
+        createHTML(html);
+      }
     })
+}
+
+//  function to create the folder for the 'team.html' file (if needed), and to create (or overwrite) the file
+function createHTML(html) {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
   }
+  fs.writeFile(outputPath, render(html), function (err) {
+    if (err) throw err;
+    console.log('File created successfully')
+  })
+}
 
-  addIntern(){
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: 'Enter Name:',
-        name: 'name'      
-      },
-      {
-        type: 'input',
-        message: 'Enter ID #:',
-        name: 'id'
-      },
-      {
-        type: 'input',
-        message: 'Enter e-mail address:',
-        name: 'email'
-      },
-      {
-        type: 'input',
-        message: 'Enter school name:',
-        name: 'school'
-      },
-    ]).then(async ({ name, id, email, school }) => {
-      
+// call this function to start the app
+start();
 
-      this.start()
-    })
-  };
-
-
-
-};
-
-  
-
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
